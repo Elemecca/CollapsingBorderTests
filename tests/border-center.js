c74f5079c6db2923fc1f9fae10437079e228ee39
Sam Hanes <sam@maltera.com>
2012-03-28 19:37:09 -0700

    Add centering tests for 1px borders.

--- tests/border-center.js
+++ tests/border-center.js
@@ -24,6 +24,28 @@ function promptVert32() {
         });
 }
 
+function promptHoriz12() {
+    Runner.inputChooseImage([
+            {   image: 'border-center/horiz-1-2-up.png',
+                title: 'drawn in upper cell' },
+            {   image: 'border-center/horiz-1-2-down.png',
+                title: 'drawn in lower cell' }
+        ], function (choice) {
+            log( choice.title );
+        });
+}
+
+function promptVert12() {
+    Runner.inputChooseImage([
+            {   image: 'border-center/vert-1-2-left.png',
+                title: 'drawn in left cell' },
+            {   image: 'border-center/vert-1-2-right.png',
+                title: 'drawn in right cell' }
+        ], function (choice) {
+            log( choice.title );
+        });
+}
+
 
 suite.addTest( "top 3px local", function() {
     table.cc.style.borderTop = '3px solid black';
@@ -104,6 +126,85 @@ suite.addTest( "right 3px both", function() {
     promptVert32();
 });
 
+
+suite.addTest( "top 1px local", function() {
+    table.cc.style.borderTop = '1px solid black';
+    table.ec.style.borderTop = '2px solid black';
+    promptHoriz12();
+});
+
+suite.addTest( "top 1px remote", function() {
+    table.nc.style.borderBottom = '1px solid black';
+    table.ec.style.borderTop = '2px solid black';
+    promptHoriz12();
+});
+
+suite.addTest( "top 1px both", function() {
+    table.cc.style.borderTop = '1px solid black';
+    table.nc.style.borderBottom = '1px solid black';
+    table.ec.style.borderTop = '2px solid black';
+    promptHoriz12();
+});
+
+
+suite.addTest( "bottom 1px local", function() {
+    table.cc.style.borderBottom = '1px solid black';
+    table.ec.style.borderBottom = '2px solid black';
+    promptHoriz12();
+});
+
+suite.addTest( "bottom 1px remote", function() {
+    table.sc.style.borderTop = '1px solid black';
+    table.ec.style.borderBottom = '2px solid black';
+    promptHoriz12();
+});
+
+suite.addTest( "bottom 1px both", function() {
+    table.cc.style.borderBottom = '1px solid black';
+    table.sc.style.borderTop = '1px solid black';
+    table.ec.style.borderBottom = '2px solid black';
+    promptHoriz12();
+});
+
+
+suite.addTest( "left 1px local", function() {
+    table.cc.style.borderLeft = '1px solid black';
+    table.sc.style.borderLeft = '2px solid black';
+    promptVert12();
+});
+
+suite.addTest( "left 1px remote", function() {
+    table.wc.style.borderRight = '1px solid black';
+    table.sc.style.borderLeft = '2px solid black';
+    promptVert12();
+});
+
+suite.addTest( "left 1px both", function() {
+    table.cc.style.borderLeft = '1px solid black';
+    table.wc.style.borderRight = '1px solid black';
+    table.sc.style.borderLeft = '2px solid black';
+    promptVert12();
+});
+
+
+suite.addTest( "right 1px local", function() {
+    table.cc.style.borderRight = '1px solid black';
+    table.sc.style.borderRight = '2px solid black';
+    promptVert12();
+});
+
+suite.addTest( "right 1px remote", function() {
+    table.ec.style.borderLeft = '1px solid black';
+    table.sc.style.borderRight = '2px solid black';
+    promptVert12();
+});
+
+suite.addTest( "right 1px both", function() {
+    table.cc.style.borderRight = '1px solid black';
+    table.ec.style.borderLeft = '1px solid black';
+    table.sc.style.borderRight = '2px solid black';
+    promptVert12();
+});
 
 })(); // end anonymous closure scope
 // vim: se sts=4 sw=4 et :miv
